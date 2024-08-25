c5f1fa8af846ea1ce6de9208a99afd8a56146055
luisfer200 <luisfernandoolmosberrio56@gmail.com>
2024-08-24 20:37:50 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'carrito',
+  webDir: 'www'
+};
+
+export default config;
